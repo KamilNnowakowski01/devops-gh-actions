b038862da9330480f72762233f7e2b4f7252fb23
Kamil Nowakowski <Nowakooo26x@gmail.com>
2025-01-07 16:46:25 +0100

    edit content

--- src/app/HomeText.tsx
+++ src/app/HomeText.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function HomeText() {
-  return (<h1>Page created for exercises in setting up github actions.</h1>);
+  return (
+  <>
+    <h1>Page created for exercises in setting up github actions.</h1>
+    <span><code>testing gh-actions</code></span>
+  </>
+);
 };
